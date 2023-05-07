8f5f961090e1c82603a118c154c93ed9f3ef920c
Anna <anya_zhevnenko@mail.ru>
2023-05-07 18:39:26 +0300

    решение задачи 15

--- Program.cs
+++ Program.cs
@@ -61,3 +61,24 @@
 // System.Console.WriteLine("Третьей цифры нет");
 // else
 // System.Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// int Prompt(string msg)
+// {
+
+// System.Console.WriteLine($"{msg}");
+// return Convert.ToInt32(Console.ReadLine());
+// }
+// int number = Prompt("Введите число");
+// string ss = "Это не день недели";
+// string check(int number)
+// {
+// if (number >= 5 && number < 8) ss = "да";
+// if (number >= 1 && number < 6) ss = "нет";
+// return ss;
+// }
+// System.Console.WriteLine(check(number));
